81d938059a281248a3c98b47b796fc1804b25783
Stefano <stefanobianchi314@gmail.com>
2023-03-12 09:57:10 +0100

    Major update: 1. NetVR now uses blender scripts to produce output. 2. OBJ objects now do not belong to classes, but I created a file 'obj_manipulation.py" that is essentially a formulary.
    In obj manipulation we have now the possibility to draw NUMPY MASKS as objects!
    This is useful for brain volumes and simple array visualization for students!

--- NetVR.py
+++ NetVR.py
@@ -1,168 +1,272 @@
+import bpy
+
 import numpy as np
-import networkx as nx
-import os
-import trimesh
+import mathutils
+from mathutils import Vector
 
-# IDEAS: FREE FORM CURVES FOR NODES AND LINKS? - ste
+from utils.canvas_manipulation import reset_canvas
+from utils.files_reader import read_coordinates, read_edgelist
 
-class NetVR:
 
-    def __init__(self, nx_graph):
+class Edge:
+    """
+    Description
+    -----------
+    Edge element class. This class creates an object that embeds all the properties of a link in a network.
+    To be developed.
+    """
+
+    def __init__(self, extremes, weight=1., scale=0.01, name='link'):
         
-        self.G = nx_graph
-        self.nodes = nx_graph.nodes(data=True)
-        self.edges = nx_graph.edges(data=True)
-        self.mesh = None
-        self.centers = None
+        self.position = extremes
+        self.weight = weight
+        self.size = self.weight*scale
+        self.name = name
+        
+        self.b_object = None
+        
+        self.attributes = None
+
+class Node:
+    """
+    Description
+    -----------
+    Node element class. This class creates an object that embeds all the properties of a node in a network.
+    To be developed.
+    """
+
+    def __init__(self, pos, size=1.0, name='node', color = [1.,1.,1.,1.]):
+
+        self.position = pos
+        self.size = size
+        self.name = name
+        
+        self.b_object = None
+        
+        self.color = tuple(color)
+        self.attributes = None
+        
+        
+
+class Network:
     
-    def _set_nodes_position(self, N, r, scale=50):
-        """
-        Description
-        -----------
-        Generate N points in space that are at least distant 2*r with each other. This is just a way
-        to create N non-overlapping spheres in a 3D space.
-
-        Parameters
-        ----------
-        N : int
-            Number of spheres to be represented.
-        r : float, np.array
-            Radius of each sphere. If a list is given, then the distances will vary accordingly.
-        scale : int
-            Scale factor that expands the dimension of the 3D space according to the maximum
-            radius value.
-
-        Returns
-        -------
-        points : numpy.array
-            Points coordinates array. Each row is a point, the columns are in order (x,y,z). 
-        """
-
-        points = np.zeros((N, 3)) # Coordinates initialization
-        box = 0 # 3D space dimension
+    def __init__(self, nodes_coords, edgelist, radii = 1.0, weights = 1.0,
+                 n_colors = None, e_colors = None):
+        self.n_pos = nodes_coords - np.mean(nodes_coords, axis=0)
+        self.e_list = edgelist
         
-        # Check if r is already a list
-        if isinstance(r, np.ndarray):
-            box=np.max(r)*scale
-        else:
-            box = r*scale
-            r = [r]*N
-
-        i = 0
-        while i < N:
-            # Generate random point in the box
-            point = box*np.random.rand(3)
-
-            # Check if the distance between the point and the rest is
-            if i == 0 or np.min(np.linalg.norm(points[:i,:] - point, axis=1)) >= (r[i]+np.max(r[:i])):
-
-                points[i,:] = point
-                i += 1
-
-        return points
-
-    def create_mesh_model(self, radius = 1.0, width = 0.05, path='network_model.obj', save=True, pos=None):
-        '''
-        Description
-        -----------
-        Generate a mesh of the network. This will create a 3D model of the network, each node and edge will be a group, while the
-        entire network is the object.
-
-        Parameters
-        ----------
-        radius : float, list, optional
-            Specify the radius of the spheres corresponding to the nodes. If the a list of radii is given, the nodes will be
-            resized accordingly.
-        width : float, list, optional
-            Specify the width of the edges. If the list of edges widths is given, all the links will be resized accordingly.
-        path : str, optional
-            Path where to save the .OBJ object.
-        save : bool, optional
-            Specify if export the .OBJ model.
-        pos : any, optional
-            Specify the nodes positions in space. If it is not given, the nodes will be placed randomly and won't overlapp
-            with each other.
-        '''
-
-        # Check if the nodes sizes are given
-        if isinstance(radius, list):
+        self.e_pos = None
+        self._get_e_pos()
+        
+        if isinstance(radii,list):
             pass
         else:
-            radius = [radius]*len(self.nodes)
-
-        # Check if the position is given
-        if pos is None:
-            self.centers = self._set_nodes_position(len(self.nodes), radius)
-        else:
-            self.centers = pos
-
-        # Add position attribute to network
-        mapp = {key:{k:v for (k,v) in zip(['x_pos','y_pos','z_pos'],[x_pos,y_pos,z_pos])} for 
-                (key,x_pos,y_pos,z_pos) in zip(self.G.nodes(), self.centers[:,0], self.centers[:,1], self.centers[:,2])}
+            radii = [radii]*len(self.n_pos)
         
-        nx.set_node_attributes(self.G, mapp)
-
-        del mapp # Free space
-
-        # Create .OBJ file
-        with open(path, 'w') as f:
+        if isinstance(weights,list):
+            pass
+        else:
+            weights = [weights]*len(self.e_pos)
             
-            f.write('# Generated with NetVR https://github.com/Stefano314/NetVR \n\n')
+        if n_colors is None:
+            n_colors = np.ones((len(self.n_pos),4))
+        else:
+            pass
             
-            ind_obj = 1 # Node number
-            ind_edges = 1 # Edge number
-            n_vertices = 0 # Number of vertices in the mesh
+        self.edges = [Edge(e, w) for e, w in zip(self.e_pos, weights)]
+        self.nodes = [Node(pos=n, size=r, color=c) for n, r, c in zip(self.n_pos, radii, n_colors)]
+        
+#        self.assign_textures(path = "C:/Users/stefa/Desktop/brain_graph.png")
+    
+        self.new = True #unused
+    
+    def assign_textures(self, path : str, mat_name = 'test_mat', tex_name = 'test_tex'):
+        # Create a new material
+        self.mats = bpy.data.materials.new(name=mat_name)
+        self.mats.use_nodes = True
+        
+#        # Create a new image texture
+#        self.tex = bpy.data.textures.new(name=tex_name, type='IMAGE')
+#        image = bpy.data.images.load(path)
+#        self.tex.image = image
 
-            f.write(f'o {path[:-4]}\n') # Object name
-            for point, r in zip(self.centers, radius):
+#        # Create a texture slot in the material
+#        slot = self.mats.texture_slots.add()
+#        slot.texture = tex
+#        slot.texture_coords = 'UV'
+        
 
-                # Create spheres as nodes
-                sphere = trimesh.primitives.Sphere(radius=r, center=[point[0],point[1],point[2]], subdivisions=0)
-                
-                f.write(f"g node {ind_obj}\n")
+    def _get_e_pos(self):
+        """
+        Description
+        -----------
+        Evaluates the position of the center of the edges. This is needed when using the rotation of the links,
+        since the rotation has the link center as rotation axis (by default, can be changed).
+        """
 
-                for row in sphere.vertices:
-                    f.write(f"v {np.round(row[0],5)} {np.round(row[1],5)} {np.round(row[2],5)}\n")
-                # self.centers.append(sphere.center)
-                
-                for row in sphere.faces:
-                    f.write(f"f {row[0]+n_vertices+1} {row[1]+n_vertices+1} {row[2]+n_vertices+1}\n")
-
-                ind_obj += 1
-                n_vertices += len(sphere.vertices)
-
-            for e in self.G.edges():
-                
-                # Define edges as boxes
-                line = trimesh.creation.box(extents=(0.1, 0.1, 1))
-
-                # Box trasformation to the vertices relative to the centers of the connected nodes 
-                line.vertices[0] = [self.centers[e[0]-1,0]+width, self.centers[e[0]-1,1]+width, self.centers[e[0]-1,2]]
-                line.vertices[1] = [self.centers[e[1]-1,0]+width, self.centers[e[1]-1,1]+width, self.centers[e[1]-1,2]]
-                line.vertices[2] = [self.centers[e[0]-1,0]-width, self.centers[e[0]-1,1]-width, self.centers[e[0]-1,2]]
-                line.vertices[3] = [self.centers[e[1]-1,0]-width, self.centers[e[1]-1,1]-width, self.centers[e[1]-1,2]]
-                line.vertices[4] = [self.centers[e[0]-1,0]+width, self.centers[e[0]-1,1]+width, self.centers[e[0]-1,2]]
-                line.vertices[5] = [self.centers[e[1]-1,0]+width, self.centers[e[1]-1,1]+width, self.centers[e[1]-1,2]]
-                line.vertices[6] = [self.centers[e[0]-1,0]-width, self.centers[e[0]-1,1]-width, self.centers[e[0]-1,2]]
-                line.vertices[7] = [self.centers[e[1]-1,0]-width, self.centers[e[1]-1,1]-width, self.centers[e[1]-1,2]]
+        self.e_pos = np.zeros((len(self.e_list),3))
+        
+        for ind,e in enumerate(self.e_list):
             
-                f.write(f'g edge {ind_edges}\n')
-                
-                for row in line.vertices:
-                    f.write(f"v {np.round(row[0],5)} {np.round(row[1],5)} {np.round(row[2],5)}\n")
+            self.e_pos[ind] = ( (self.n_pos[e[0]][0]+self.n_pos[e[1]][0])/2.,\
+                                (self.n_pos[e[0]][1]+self.n_pos[e[1]][1])/2.,\
+                                (self.n_pos[e[0]][2]+self.n_pos[e[1]][2])/2. )
+    
+    # def _generate_edge_objects(self):
+        
+    #     pass
+                                
+    # def _generate_node_objects(self):
+    #     # Check if the nodes sizes are given
+    #     if isinstance(radius, list):
+    #         pass
+    #     else:
+    #         radius = [radius]*len(self.nodes)
+       
+    def translate_to_point(self, point, draw=True):
+        """
+        Description
+        -----------
+        Translate and redraw the whole network in a specific point. The point will be the new network center.
+        """
 
-                for row in line.faces:
-                    f.write(f"f {row[0]+n_vertices+1} {row[1]+n_vertices+1} {row[2]+n_vertices+1}\n")
+        point = np.array(point)
+        vec = point - np.mean(self.n_pos, axis=0)
+        self.n_pos += vec
+        self.e_pos += vec
+        
+        for ind,e in enumerate(self.edges):
+            e.position = self.e_pos[ind]
+            
+        for ind,n in enumerate(self.nodes):
+            n.position = self.n_pos[ind]
+            
+        if draw:
+            reset_canvas(all=False)
+            self.draw_network()
+    
+    
+    def scale_network(self, factor=2.):
+        """
+        Description
+        -----------
+        Scale network by a specified factor.
+        """
+        center = np.mean(self.n_pos, axis=0)
+        self.translate_to_point([0.,0.,0.], draw=False)
+        
+        self.n_pos *= factor
+        self.e_pos *= factor
+        self.n_pos += center
+        self.e_pos += center
 
-                n_vertices += len(line.vertices)
-                ind_edges+=1
+        for ind,e in enumerate(self.edges):
+            e.position = self.e_pos[ind]
+            e.size *= factor
+            
+        for ind,n in enumerate(self.nodes):
+            n.position = self.n_pos[ind]
+            n.size *= factor
+            
+        reset_canvas(all=False)
+        self.draw_network()
+        
+    
+    def draw_nodes(self):
 
-        # Create mesh object
-        self.mesh = trimesh.load(path, force='mesh')
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.ops.mesh.primitive_uv_sphere_add(segments=18, ring_count=16, radius=1.)
+        primitive_node = bpy.context.object
+        
+        for ind, center in enumerate(self.n_pos):
+            
+            node_size = self.nodes[ind].size
+            
+            # Create a new link_ object and link it to the mesh data block
+            node_ = primitive_node.copy()
+            node_.data = primitive_node.data.copy()
+            
+            node_.location = (self.n_pos[ind,0],self.n_pos[ind,1],self.n_pos[ind,2])
+            node_.scale = (node_size, node_size, node_size)
+            node_.name = f'node_{ind}'
+            
+            # Color
+           
+            mat = bpy.data.materials.new(name=f'node_{ind}')
+            mat.diffuse_color = self.nodes[ind].color
+            # Assign the material to the object
+            if node_.data.materials:
+                node_.data.materials[0] = mat
+            else:
+                node_.data.materials.append(mat)
 
-        # Delete file if not required
-        if not save:
-            os.remove(path)
+            # Memorize object properties
+            self.nodes[ind].name = f'node_{ind}'
+            self.nodes[ind].b_object = node_
+            
+            bpy.context.scene.collection.objects.link(node_)
+            
+        # Remove primitive object
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.data.objects.get('Sphere').select_set(True)
+        bpy.ops.object.delete()
+        
+#            bpy.context.view_layer.objects.active = node_
 
-    def export_to_obj(self, path : str, **kwargs):
-        trimesh.exchange.export.export_mesh(self.mesh, path, kwargs, file_type='obj')
+#            node_.active_material.diffuse_color = self.nodes[ind].color
+            
+#            bpy.data.materials.new(name=f"mat_node_{ind}")
+#            bpy.data.materials[f"mat_node_{ind}"].diffuse_color = self.nodes[ind].color
+#            bpy.data.materials[f"mat_node_{ind}"].specular_intensity = 0.5
+#            mat.use_nodes = True
+#            mat_nodes = mat.node_tree.nodes
+#            mat_links = mat.node_tree.links
+#            node_.data.materials.colors
+#            mat_nodes['Principled BSDF'].inputs['Base Color'].default_value = self.nodes[ind].color
+            
+            
+    
+    def draw_edges(self):
+        
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.ops.mesh.primitive_cone_add(vertices=12, radius1=1., radius2=1., depth=1.)
+        primitive_link = bpy.context.object
+
+        # Create links
+        for ind,e in enumerate(self.e_list):
+            
+            e_weight = self.edges[ind].size
+            v = self.n_pos[e[0]] - self.n_pos[e[1]]
+            link_direction = Vector(v)
+            link_length = link_direction.length
+            link_direction.normalize()
+            
+            # Create a new link_ object and link it to the mesh data block
+            link_ = primitive_link.copy()
+            link_.data = primitive_link.data.copy()
+            
+            link_.location = self.e_pos[ind]
+            
+            link_.scale = (e_weight, e_weight, link_length)
+            link_.name = f'link_{ind}'
+            
+            bpy.context.scene.collection.objects.link(link_)
+            
+            bpy.context.view_layer.objects.active = link_
+
+            bpy.context.object.rotation_mode = 'QUATERNION'
+            bpy.context.object.rotation_quaternion = link_direction.to_track_quat('Z','Y')
+            
+            # Memorize object properties
+            self.edges[ind].name = f'link_{ind}'
+            self.edges[ind].b_object = link_
+            
+        # Remove primitive object
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.data.objects.get('Cone').select_set(True)
+        bpy.ops.object.delete()
+
+
+    def draw_network(self):
+        self.draw_nodes()
+        self.draw_edges()
---
+++ obj_manipulation.py
@@ -0,0 +1,250 @@
+import numpy as np
+import networkx as nx
+import os
+import trimesh
+
+# IDEAS: FREE FORM CURVES FOR NODES AND LINKS? - ste
+
+def is_valid_index(index, A):
+    return all(0 <= i < A.shape[0] for i in index)
+    
+def _set_nodes_position(N, r, scale=50):
+    """
+    Description
+    -----------
+    Generate N points in space that are at least distant 2*r with each other. This is just a way
+    to create N non-overlapping spheres in a 3D space.
+    Parameters
+    ----------
+    N : int
+        Number of spheres to be represented.
+    r : float, np.array
+        Radius of each sphere. If a list is given, then the distances will vary accordingly.
+    scale : int
+        Scale factor that expands the dimension of the 3D space according to the maximum
+        radius value.
+    Returns
+    -------
+    points : numpy.array
+        Points coordinates array. Each row is a point, the columns are in order (x,y,z). 
+    """
+    points = np.zeros((N, 3)) # Coordinates initialization
+    box = 0 # 3D space dimension
+    
+    # Check if r is already a list
+    if isinstance(r, np.ndarray):
+        box=np.max(r)*scale
+    else:
+        box = r*scale
+        r = [r]*N
+    i = 0
+    while i < N:
+        # Generate random point in the box
+        point = box*np.random.rand(3)
+        # Check if the distance between the point and the rest is
+        if i == 0 or np.min(np.linalg.norm(points[:i,:] - point, axis=1)) >= (r[i]+np.max(r[:i])):
+            points[i,:] = point
+            i += 1
+    return points
+
+def array_mask_mesh(A : np.ndarray, radius=1., width=0.05, edges = False, path : str='test.obj', volume='cube'):
+    """
+    Create a 3D object of a (N,M,L) array mask.
+    If volume = 'cube', edges=False and radius=1, then the boxes will share faces, producing a real volume mask,
+    but it's harder to understand visually.
+    """
+    centers = np.argwhere(A)
+    radius = [radius]*centers.shape[0]
+    
+    mesh = None
+
+    with open(path, 'w') as f:
+                
+        f.write('# Generated with NetVR https://github.com/Stefano314/NetVR \n\n')
+        
+        ind_obj = 1 # Node number
+        ind_edges = 1 # Edge number
+        n_vertices = 0 # Number of vertices in the mesh
+
+        f.write(f'o {path[:-4]}\n') # Object name
+        for point, r in zip(centers, radius):
+            
+            # Create spheres as nodes
+            if volume == 'cube':
+                # Construct the translation matrix
+                translation_matrix = np.array([[1, 0, 0, point[0]], 
+                                               [0, 1, 0, point[1]], 
+                                               [0, 0, 1, point[2]], 
+                                               [0, 0, 0, 1]])
+                mesh = trimesh.creation.box(extents=(r, r, r), transform=translation_matrix)
+            elif volume == 'sphere':
+                mesh = trimesh.primitives.Sphere(radius=r, center=[point[0],point[1],point[2]], subdivisions=0)
+
+            f.write(f"g node {ind_obj}\n")
+
+            for row in mesh.vertices:
+                f.write(f"v {np.round(row[0],5)} {np.round(row[1],5)} {np.round(row[2],5)}\n")
+            
+            for row in mesh.faces:
+                f.write(f"f {row[0]+n_vertices+1} {row[1]+n_vertices+1} {row[2]+n_vertices+1}\n")
+
+            ind_obj += 1
+            n_vertices += len(mesh.vertices)
+
+        if edges:
+
+            # Iterate over all coordinates and check adjacent elements
+            edgelist = []
+            # center_index = 0
+            
+            for index in centers:
+                starting_index = np.where(np.all(centers==list(index), axis=1))[0][0]
+                for offset in [(0, 0, 1), (0, 0, -1), (0, 1, 0), (0, -1, 0), (1, 0, 0), (-1, 0, 0)]:
+                    adjacent_index = tuple(index + offset)
+                    if is_valid_index(adjacent_index, A) and A[adjacent_index]:
+                        edgelist.append((starting_index, np.where(np.all(centers==list(adjacent_index), axis=1))[0][0]))
+
+            # edgelist = [(e[0]-1,e[1]-1) for e in edgelist] # In case we don't start from zero position
+            for e in edgelist:
+                
+                line = trimesh.creation.cylinder(radius=width, segment=[centers[e[0]],centers[e[1]]], sections=4)
+            
+                f.write(f'g edge {ind_edges}\n')
+                
+                for row in line.vertices:
+                    f.write(f"v {np.round(row[0],5)} {np.round(row[1],5)} {np.round(row[2],5)}\n")
+
+                for row in line.faces:
+                    f.write(f"f {row[0]+n_vertices+1} {row[1]+n_vertices+1} {row[2]+n_vertices+1}\n")
+
+                n_vertices += len(line.vertices)
+                ind_edges+=1
+
+def create_mesh_model(nodes, G, radius = 1.0, width = 0.05, path='network_model.obj', save=True, pos=None):
+    '''
+    Description
+    -----------
+    Generate a mesh of the network. This will create a 3D model of the network, each node and edge will be a group, while the
+    entire network is the object.
+    Parameters
+    ----------
+    radius : float, list, optional
+        Specify the radius of the spheres corresponding to the nodes. If the a list of radii is given, the nodes will be
+        resized accordingly.
+    width : float, list, optional
+        Specify the width of the edges. If the list of edges widths is given, all the links will be resized accordingly.
+    path : str, optional
+        Path where to save the .OBJ object.
+    save : bool, optional
+        Specify if export the .OBJ model.
+    pos : any, optional
+        Specify the nodes positions in space. If it is not given, the nodes will be placed randomly and won't overlapp
+        with each other.
+    '''
+    # Check if the nodes sizes are given
+    if isinstance(radius, list):
+        pass
+    else:
+        radius = [radius]*len(nodes)
+    # Check if the position is given
+    if pos is None:
+        centers = _set_nodes_position(len(nodes), radius)
+    else:
+        centers = pos
+    # Add position attribute to network
+    mapp = {key:{k:v for (k,v) in zip(['x_pos','y_pos','z_pos'],[x_pos,y_pos,z_pos])} for 
+            (key,x_pos,y_pos,z_pos) in zip(G.nodes(), centers[:,0], centers[:,1], centers[:,2])}
+    
+    nx.set_node_attributes(G, mapp)
+    del mapp # Free space
+    # Create .OBJ file
+    with open(path, 'w') as f:
+        
+        f.write('# Generated with NetVR https://github.com/Stefano314/NetVR \n\n')
+        
+        ind_obj = 1 # Node number
+        ind_edges = 1 # Edge number
+        n_vertices = 0 # Number of vertices in the mesh
+        f.write(f'o {path[:-4]}\n') # Object name
+        for point, r in zip(centers, radius):
+            # Create spheres as nodes
+            sphere = trimesh.primitives.Sphere(radius=r, center=[point[0],point[1],point[2]], subdivisions=3)
+            
+            f.write(f"g node {ind_obj}\n")
+            for row in sphere.vertices:
+                f.write(f"v {np.round(row[0],5)} {np.round(row[1],5)} {np.round(row[2],5)}\n")
+            # centers.append(sphere.center)
+            
+            for row in sphere.faces:
+                f.write(f"f {row[0]+n_vertices+1} {row[1]+n_vertices+1} {row[2]+n_vertices+1}\n")
+            ind_obj += 1
+            n_vertices += len(sphere.vertices)
+
+        for e in G.edges():
+            # -1 is for edges in networkx
+            line = trimesh.creation.cylinder(radius=width, segment=[centers[e[0]-1],centers[e[1]-1]], sections=4)
+        
+            f.write(f'g edge {ind_edges}\n')
+            
+            for row in line.vertices:
+                f.write(f"v {np.round(row[0],5)} {np.round(row[1],5)} {np.round(row[2],5)}\n")
+            for row in line.faces:
+                f.write(f"f {row[0]+n_vertices+1} {row[1]+n_vertices+1} {row[2]+n_vertices+1}\n")
+            n_vertices += len(line.vertices)
+            ind_edges+=1
+    # # Create mesh object
+    # mesh = trimesh.load(path, force='mesh')
+    # Delete file if not required
+    if not save:
+        os.remove(path)
+
+
+import pandas as pd
+
+df =pd.read_csv('test_edgelist.csv')
+
+df['type'] = 0
+df['type'][:300] = 'type1'
+df['type'][300:] = 'type2'
+df = df[['source', 'target', 'number_of_fibers', 'type']]
+df.to_csv('test_edgelist_types.csv', header=True, index=False)
+
+def read_coordinates(path : str, header = True):
+    a = []
+    coords = []
+    categories = []
+
+    with open(path, 'r') as f:
+        a = [line.replace('\n','') for line in f]
+
+
+    if len(a[0].split(',')) == 4:
+        # csv: x,y,z,category
+        if header:
+            coords = np.array([[float(i.split(',')[0]),
+                            float(i.split(',')[1]),
+                            float(i.split(',')[2])] for i in a[1:]])
+            
+            categories = [str(i.split(',')[3]) for i in a[1:]]
+
+        else:
+            coords = np.array([[float(i.split(',')[0]),
+                            float(i.split(',')[1]),
+                            float(i.split(',')[2])] for i in a])
+            
+            categories = [str(i.split(',')[3]) for i in a]
+
+    else:
+        # csv: x,y,z coordinates
+        categories = ['white']*len(a)
+
+        if header:
+            coords = np.array([[float(i.split(',')[0]),
+                            float(i.split(',')[1]),
+                            float(i.split(',')[2])] for i in a[1:]])
+        else:
+            coords = np.array([[float(i.split(',')[0]),
+                            float(i.split(',')[1]),
+                            float(i.split(',')[2])] for i in a])
+    return coords, categories
+        
